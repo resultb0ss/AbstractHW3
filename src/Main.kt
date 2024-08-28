ea337b9158ae03b2255b136beb81c0920c0df8a8
Admin <reteamboss@gmail.com>
2024-08-28 11:56:28 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+
+    var numOperation = 2
+    var num: Double = 10.0
+
+    when(numOperation){
+        1 -> println("${num * 10} дециметров")
+        2 -> println("${num / 1000} километров")
+        3 -> println("${num} метров")
+        4 -> println("${num * 1000} милимметров")
+        5 -> println("${num * 100} сантиметров")
+    }
 }
